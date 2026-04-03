6aa8c91af16203b0f12269591f8086bfa77468b6
pmq72002 <pmqchoke@gmail.com>
2026-04-03 23:55:42 +0700

    update

--- src/app/DashBoard/dashBoard.component.ts
+++ src/app/DashBoard/dashBoard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {ChartConfiguration, ChartData, ChartOptions, ChartType} from "chart.js";
+import { ChartData, ChartOptions, ChartType} from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 interface Category {
   name: string;
@@ -14,9 +15,10 @@ interface Category {
 })
 export class DashboardComponent {
 
-  pieChartType: ChartType = 'pie';
+  pieChartType: ChartType = 'doughnut';
   totalBudget = 6800000;
   totalRemaining = 3200000;
+  pieChartPlugins = [ChartDataLabels];
 
   categories: Category[] = [
     { name: 'Ăn uống', spent: 800000, limit: 1500000 },
@@ -24,7 +26,7 @@ export class DashboardComponent {
     { name: 'Cầu lông', spent: 300000, limit: 400000 },
     { name: 'Bi-a', spent: 600000, limit: 400000 },
     { name: 'Thuốc lá', spent: 200000, limit: 300000 },
-    { name: 'Mua sắm', spent: 100000, limit: 500000 }
+    { name: 'Mua sắm', spent: 100000, limit: 500000 },
   ];
 
   getPercent(c: Category): number {
@@ -40,38 +42,46 @@ export class DashboardComponent {
 
   getColor(c: Category): string {
     const percent = (c.spent / c.limit) * 100;
-
-    if (percent < 70) return 'bg-green';
-    if (percent < 100) return 'bg-yellow';
+    if (percent < 50) return 'bg-green';
+    if (percent < 80) return 'bg-yellow';
     return 'bg-red';
+  }
+
+  // Dùng cho legend HTML
+  getColorBox(c: Category): string {
+    const colors = [
+      '#22c55e', '#3b82f6', '#facc15', '#ef4444', '#a855f7', '#14b8a6'
+    ];
+    const index = this.categories.indexOf(c);
+    return colors[index];
   }
 
   addExpense() {
     alert('Đi tới màn hình thêm chi tiêu');
   }
 
-  pieChartData: ChartData<'pie'> = {
+  pieChartData: ChartData<'doughnut'> = {
     labels: this.categories.map(c => c.name),
     datasets: [
       {
         data: this.categories.map(c => c.spent),
         backgroundColor: [
-          '#22c55e', // ăn uống
-          '#3b82f6', // xăng
-          '#facc15', // cầu lông
-          '#ef4444', // bi-a
-          '#a855f7', // thuốc lá
-          '#14b8a6'  // mua sắm
-        ]
+          '#22c55e', '#3b82f6', '#facc15', '#ef4444', '#a855f7', '#14b8a6'
+        ],
+        borderWidth: 1,
+        borderColor: '#ffffff',
+        spacing: 5
       }
     ]
   };
-  pieChartOptions: ChartOptions<'pie'> = {
+
+  pieChartOptions: ChartOptions<'doughnut'> = {
     responsive: true,
+    maintainAspectRatio: false, // cho canvas to lên
+    cutout: '65%',
     plugins: {
-      legend: {
-        position: 'bottom'
-      }
+      legend: { display: false }, // tắt legend chart, dùng HTML
+      datalabels: { display: false } // tắt label trồi ra ngoài
     }
   };
 }
